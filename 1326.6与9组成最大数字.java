31232229ac4c91f605e638df9c660008e2f9440a
hohaiyth <55796172+hohaiyth@users.noreply.github.com>
2020-11-09 14:44:37 +0800

    Create 1326.6与9组成最大数字.java

---
+++ 1326.6与9组成最大数字.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximum69Number (int num) {
+    if(num/1000 == 6){
+        return num+3000;
+    }
+    else if((num/100)%10 == 6){
+        return num+300;
+    }
+    else if ((num/10)%10 == 6){
+        return num+30;
+    }
+    else if (num%10 == 6){
+        return num+3;
+    }
+    return num;
+    }
+}
